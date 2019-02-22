7953a8ff1ded114ae2889ec30859183553d5d759
tylers1st <47163941+tylers1st@users.noreply.github.com>
2019-02-22 11:10:50 -0600

    Update array_utils.h
    To determine the i-ith smallest number of an array.

--- array_utils.h
+++ array_utils.h
@@ -21,12 +21,21 @@ void reverse(int *arr, int size) {
 int orderStatistic(int *arr, int size, int i){
    int element = -1;
    //todo
-
+   int min = arr[0];
+   int prevMin = min;
+   for (int iteration = 0; i < size; iteration++){  // Used for assignment of the ith value
+      for (int j = 0; j < size; j++){ // Used for determining the next ith value
+         if (arr[j] < min && arr[j] > prevMin){
+            min = arr[j]
+         }
+      }
+   }
    return element;
 }
 
 int main(){
    //you may choose to do your testing here
-   
+   int array[10] = {1,2,3,4,5,6,7,8,9,10};
+   printf("%d", orderStatistic(array, 10, 1));
    return 0;
 }

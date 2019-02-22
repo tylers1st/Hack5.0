950b1739fe6ed2bd9a84fcd08e4a6b11e67ae08b
tylers1st <47163941+tylers1st@users.noreply.github.com>
2019-02-22 10:30:17 -0600

    Create array_utils.h

---
+++ array_utils.h
@@ -0,0 +1,32 @@
+#include<stdlib.h>
+
+#include "array_utils.h"
+
+int containsWithin(const int *arr, int size, int x, int i, int j) {
+   //todo
+  return 0;
+}
+
+int contains(const int *arr, int size, int x) {
+   
+   //todo
+  return 0;
+}
+
+void reverse(int *arr, int size) {
+   //todo
+  return;
+}
+
+int orderStatistic(int *arr, int size, int i){
+   int element = -1;
+   //todo
+
+   return element;
+}
+
+int main(){
+   //you may choose to do your testing here
+   
+   return 0;
+}

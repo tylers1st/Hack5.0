a483053d3ea249f08b40e5778260e1d7c7b5ce3b
tylers1st <47163941+tylers1st@users.noreply.github.com>
2019-02-22 13:33:17 -0600

    Update array_utils.h

--- array_utils.h
+++ array_utils.h
@@ -22,22 +22,26 @@ void reverse(int *arr, int size) {
 int orderStatistic(int *arr, int size, int i){
    int element = -1;
    //todo
-   int index;
+   int index = 0;
    int min = arr[0];
    int prevMin = -1;
-      for (int j = 0, index = 0; j < i; ++j, index++){
-      printf("\n\nj = %d\n\n", j);
-      printf("\nFirst for loop\n");
-      printf("min = %d, prevMin = %d, arr[index] = %d\n", min, prevMin, arr[index]);
-         if (arr[index] > prevMin){
-            min = arr[index];
-            for (index 0; index < size; index++){
-               printf("arr[%d] = %d\n",k,arr[index]);
-               if(arr[index] < min && arr[index] > prevMin){
-                  min = arr[index];
-                  printf("min = %d\n\n", min);
-                  prevMin = min;
+      for (int j = 0; j < i; ++j){
+         index = 0;
+         printf("\n\nj = %d\n\n", j);
+         printf("\nFirst for loop\n");
+         printf("min = %d, prevMin = %d, arr[index] = %d\n", min, prevMin, arr[index]);
+         for(index = 0; index < size; index++){
+            printf("Index = %d\n", index);
+            if (arr[index] > prevMin){
+                min = arr[index];
+               for (int index2 = 0; index2 < size; index2++){
+                  printf("arr[%d] = %d\n",index2,arr[index2]);
+                   if(arr[index2] < min && arr[index2] > prevMin){
+                     min = arr[index2];
+                     printf("min = %d\n\n", min);
+                  }
                }
+            prevMin = min;
             }
          }
       }
@@ -50,9 +54,9 @@ int main(){
    // int array[10] = {1,2,5,4,5,6,7,8,9,10};
    // printf("%d\n", orderStatistic(array, 10, 3));
    int array2[4] = {5,2,1,0};
-   printf("%d\n", orderStatistic(array2, 4 , 4));
+   printf("%d\n", orderStatistic(array2, 4 , 1));
    printf("\n---------------\n");
    
-   printf("%d\n", orderStatistic(array2, 4, 3));
+   printf("%d\n", orderStatistic(array2, 4, 2));
    return 0;
 }

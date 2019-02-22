7783af2f56a71696fadc61d9febb42d49382f809
tylers1st <47163941+tylers1st@users.noreply.github.com>
2019-02-22 16:10:09 -0600

    Create array_utils.c

---
+++ array_utils.c
@@ -0,0 +1,27 @@
+//write documentation
+/* 
+   containsWithin is meant to determine if a number x
+   exists within an array in between two indexes i and j.
+ */
+int containsWithin(const int *a, int size, int x, int i, int j);
+
+//write documentation
+/*
+  contains is meant to determine if a number x 
+  exists within an array.
+ */
+int contains(const int *a, int size, int x);
+
+//write documentation
+/*
+  reverse is meant to take an array and reverse
+  the order that it is stored as. 
+ */
+void reverse(int *arr, int size);
+
+//write documentation
+/*
+  orderStatistic is meant to determine the
+  i-ith smallest number in an array
+ */
+int orderStatistic(int *arr, int size, int i);
